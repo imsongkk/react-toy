eb575cfc962d3c34b149ca0e7eaeab0ea49fac5b
Imsung <isyou7487@naver.com>
2021-08-20 14:56:57 +0900

    13장

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,12 +4,20 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트의 기초', checked: true },
-    { id: 2, text: '리액트 기초', checked: true },
-    { id: 3, text: '리액트의 기초', checked: false },
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4);
 
